8d1f4b4ec341753b28cef739020fb058b018a216
Juan Pablo Rull Camacho <tempestaddepvc@gmail.com>
2015-10-29 13:46:31 +0100

    Mini practica botonera

---
+++ MiniPracticaBotoneraArrayList/MiniPracticaBotoneraArrayList/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MiniPracticaBotoneraArrayList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonAnterior = new System.Windows.Forms.Button();
+            this.buttonBorrar = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.buttonSiguiente = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre:";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(64, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_guardar);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(64, 60);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 3;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Visible = false;
+            this.buttonCancelar.Click += new System.EventHandler(this.button_cancelar);
+            // 
+            // buttonAnterior
+            // 
+            this.buttonAnterior.Location = new System.Drawing.Point(13, 353);
+            this.buttonAnterior.Name = "buttonAnterior";
+            this.buttonAnterior.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnterior.TabIndex = 4;
+            this.buttonAnterior.Text = "Anterior";
+            this.buttonAnterior.UseVisualStyleBackColor = true;
+            this.buttonAnterior.Visible = false;
+            this.buttonAnterior.Click += new System.EventHandler(this.button_anterior);
+            // 
+            // buttonBorrar
+            // 
+            this.buttonBorrar.Location = new System.Drawing.Point(120, 353);
+            this.buttonBorrar.Name = "buttonBorrar";
+            this.buttonBorrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonBorrar.TabIndex = 5;
+            this.buttonBorrar.Text = "Borrar";
+            this.buttonBorrar.UseVisualStyleBackColor = true;
+            this.buttonBorrar.Visible = false;
+            this.buttonBorrar.Click += new System.EventHandler(this.button_borrar);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(239, 353);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Nuevo";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button_nuevo);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(353, 353);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Modificar";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button_modificar);
+            // 
+            // buttonSiguiente
+            // 
+            this.buttonSiguiente.Location = new System.Drawing.Point(466, 353);
+            this.buttonSiguiente.Name = "buttonSiguiente";
+            this.buttonSiguiente.Size = new System.Drawing.Size(75, 23);
+            this.buttonSiguiente.TabIndex = 8;
+            this.buttonSiguiente.Text = "Siguiente";
+            this.buttonSiguiente.UseVisualStyleBackColor = true;
+            this.buttonSiguiente.Visible = false;
+            this.buttonSiguiente.Click += new System.EventHandler(this.button_siguiente);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.buttonCancelar);
+            this.panel1.Location = new System.Drawing.Point(341, 175);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 388);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonSiguiente);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.buttonBorrar);
+            this.Controls.Add(this.buttonAnterior);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.Button buttonAnterior;
+        private System.Windows.Forms.Button buttonBorrar;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button buttonSiguiente;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ MiniPracticaBotoneraArrayList/MiniPracticaBotoneraArrayList/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniPracticaBotoneraArrayList
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList arraylist = new ArrayList();
+        private int posicionEnArrayList = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            arraylist.Add("");
+        }
+
+
+        private void modoEdicion()
+        {
+            textBox1.ReadOnly = false;
+            button1.Visible = true;
+            buttonCancelar.Visible = true;
+            buttonAnterior.Visible = false;
+            buttonBorrar.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            buttonSiguiente.Visible = false;
+            if (arraylist.Count == 1)
+            {
+                buttonCancelar.Visible = false;
+            }
+        }
+        private void modoVision()
+        {
+            textBox1.Text = (String)arraylist[posicionEnArrayList];
+            textBox1.ReadOnly = true;
+            button1.Visible = false;
+            buttonCancelar.Visible = false;
+            buttonAnterior.Visible = true;
+            buttonBorrar.Visible = true;
+            button5.Visible = true;
+            button6.Visible = true;
+            buttonSiguiente.Visible = true;
+            comprobarBotonesMovimiento();
+        }
+
+        private void button_nuevo(object sender, EventArgs e)
+        {
+            nuevo();
+          
+
+
+        }
+        private void nuevo()
+        {
+            textBox1.ResetText();
+            posicionEnArrayList = arraylist.Count;
+            arraylist.Add("");
+            modoEdicion();
+        }
+        private void button_guardar(object sender, EventArgs e)
+        {
+      
+                arraylist[posicionEnArrayList] = textBox1.Text;
+                modoVision();
+            
+        }
+
+        private void button_cancelar(object sender, EventArgs e)
+        {
+            if (arraylist[posicionEnArrayList] == "")
+            {
+                arraylist.RemoveAt(posicionEnArrayList);
+
+            }
+            posicionEnArrayList = 0;
+            modoVision();
+        }
+
+        private void button_anterior(object sender, EventArgs e)
+        {
+            posicionEnArrayList -= 1;
+            textBox1.Text = (String)arraylist[posicionEnArrayList];
+            comprobarBotonesMovimiento();
+        }
+
+        private void button_siguiente(object sender, EventArgs e)
+        {
+            posicionEnArrayList += 1;
+            textBox1.Text = (String)arraylist[posicionEnArrayList];
+            comprobarBotonesMovimiento();
+
+        }
+
+        private void button_modificar(object sender, EventArgs e)
+        {
+            modoEdicion();
+        }
+        private void comprobarBotonesMovimiento()
+        {
+            if (posicionEnArrayList == (arraylist.Count - 1))
+            {
+                buttonSiguiente.Visible = false;
+            }
+            else
+            {
+                buttonSiguiente.Visible = true;
+            }
+            if (posicionEnArrayList == 0)
+            {
+                buttonAnterior.Visible = false;
+            }
+            else
+            {
+                buttonAnterior.Visible = true;
+            }
+        }
+
+        private void button_borrar(object sender, EventArgs e)
+        {
+            arraylist.RemoveAt(posicionEnArrayList);
+            textBox1.ResetText();
+            posicionEnArrayList = 0;
+            if (arraylist.Count ==0)
+            {
+                nuevo();
+            }
+            else
+            {
+                modoVision();
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+    }
+
+}
